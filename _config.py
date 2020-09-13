474ba244462caf8854499b17c6e9d227ce50155f
Ándreas Himmler <andy.himmler@gmail.com>
2020-09-13 07:25:38 +0200

    Config File added

---
+++ _config.py
@@ -0,0 +1,35 @@
+# RENAME TO config.py, when using the program!
+
+mates = {
+    "Number One": {
+        "google": [
+            "id",
+            "id",
+            "id"
+        ],
+        "webuntis": {
+            "username": "user",
+            "password": "pass",
+            "server": "server url",
+            "school": "school1",
+            "useragent": "WebUntis Test",
+            "surname": "One",
+            "fore_name": "Number"
+        }
+    },
+    "Number Two": {
+        "google": [
+            "id"
+        ]
+    },
+    "Number Seven": {
+        "webuntis": {
+            "username": "user",
+            "password": "pass",
+            "server": "server url",
+            "school": "school2",
+            "useragent": "WebUntis Test",
+            "class": "XY"
+        }
+    }
+}
