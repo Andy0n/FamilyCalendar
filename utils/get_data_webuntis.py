ec8e2216ef1828da3b36a1b8badff05397d86431
Andreas Himmler <andy.himmler@gmail.com>
2021-09-01 23:53:11 +0200

    WebUntis Workaround two dates in two different school years (So it doesn't crash MagicMirror)

--- utils/get_data_webuntis.py
+++ utils/get_data_webuntis.py
@@ -1,4 +1,5 @@
 import webuntis
+import sys
 
 
 def get_webuntis(cred, start, end):
@@ -12,25 +13,32 @@ def get_webuntis(cred, start, end):
         useragent=cred["useragent"]
     ).login()
 
-    if "class" in cred:
-        klasse = s.klassen().filter(name=cred["class"])[0]
-        timetable = s.timetable(start=start, end=end, klasse=klasse)
-    elif "surname" in cred and "fore_name" in cred:
-        student = s.get_student(surname=cred["surname"], fore_name=cred["fore_name"]).id
-        timetable = s.timetable(start=start, end=end, student=student)
-    else:
-        raise AttributeError("WebUntis is not configured correctly. Neither class nor surname&fore_name is configured.")
+    old_stderr = sys.stderr
+    sys.stderr = sys.stdout
 
-    timetable = sorted(timetable, key=lambda lesson: lesson.start)
+    try:
+        if "class" in cred:
+            klasse = s.klassen().filter(name=cred["class"])[0]
+            timetable = s.timetable(start=start, end=end, klasse=klasse)
+        elif "surname" in cred and "fore_name" in cred:
+            student = s.get_student(surname=cred["surname"], fore_name=cred["fore_name"]).id
+            timetable = s.timetable(start=start, end=end, student=student)
+        else:
+            raise AttributeError("WebUntis is not configured correctly. Neither class nor surname&fore_name is configured.")
 
-    for lesson in timetable:
-        if lesson.code != "cancelled":
-            st = lesson.start.hour + lesson.start.minute / 60.0
-            en = lesson.end.hour + lesson.end.minute / 60.0
+        timetable = sorted(timetable, key=lambda lesson: lesson.start)
 
-            if lesson.start.weekday() in data:
-                data[lesson.start.weekday()] += [(st, en)]
-            else:
-                data[lesson.start.weekday()] = [(st, en)]
+        for lesson in timetable:
+            if lesson.code != "cancelled":
+                st = lesson.start.hour + lesson.start.minute / 60.0
+                en = lesson.end.hour + lesson.end.minute / 60.0
 
+                if lesson.start.weekday() in data:
+                    data[lesson.start.weekday()] += [(st, en)]
+                else:
+                    data[lesson.start.weekday()] = [(st, en)]
+    except:
+        print("Start and End Date not in the same school year.")
+
+    sys.stderr = old_stderr
     return data
